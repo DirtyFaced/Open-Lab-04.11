bed23c7b0cf335734bbce8a10c8cee04dc7a3277
Jakub Dipčár <jakub.dipcar@gmail.com>
2020-11-16 00:23:14 +0100

    hurray

--- Open-Lab-04.11/StringTools.cs
+++ Open-Lab-04.11/StringTools.cs
@@ -6,7 +6,27 @@ namespace Open_Lab_04._11
     {
         public string AlphabetSoup(string str)
         {
-            throw new NotImplementedException();
+            char[] chungus = new char[str.Length];
+            for (int i = 0; i < str.Length; i++)
+            {
+                int smrad = 0;
+                for (int ii = 0; ii < str.Length; ii++)
+                {
+                    int first = (int)str[i];
+                    int second = (int)str[ii];
+                    if (first > second)
+                    {
+                        smrad = smrad + 1;
+                    }
+                }
+                for (int iii = smrad; chungus[smrad] == str[i]; iii++)
+                {
+                    smrad = smrad + 1;   
+                }
+                chungus[smrad] = str[i];
+            }
+            string vysledok = new string(chungus);
+            return vysledok;
         }
     }
 }
